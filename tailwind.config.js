db9ec4d720e41554d05342cd4956711ced4805d6
nikolakalcic <nikolacalcio@gmail.com>
2024-11-11 00:54:18 +0100

    html update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
-// tailwind.config.js
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",  // Make sure this includes the path to your React components
-    "./public/index.html"
+    './src/**/*.{html,js,jsx,ts,tsx}',  // Adjust this for your file paths
+    './public/index.html',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
